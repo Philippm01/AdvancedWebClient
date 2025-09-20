dacfd005a84b5f627e6285f81297c888d12361a1
philippm01 <philippmuro@gmail.com>
2025-09-19 17:31:12 -0700

    Lecture 4

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WeatherData } from '../weather-data';
+import { CdkAriaLive } from '@angular/cdk/a11y';
+
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [CdkAriaLive],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
+
 export class Weather {
+  forecast: WeatherData[] = [];
   constructor(http: HttpClient) {
-    http.get<WeatherData[]>('http://localhost:5201/weatherforecast').subscribe(result => {
+    http.get<WeatherData[]>('http://localhost:5201/weatherforecast').subscribe(result => {  
+      this.forecast = result;
     });
   }
 }
