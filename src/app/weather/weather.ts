1a7b9d1c4328f966bf46f327e2472d0a670a3b3b
philippm01 <philippmuro@gmail.com>
2025-11-07 18:23:15 -0800

    week 11

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -2,19 +2,21 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WeatherData } from '../weather-data';
 import { CdkAriaLive } from '@angular/cdk/a11y';
+import { Observable } from 'rxjs';
+import { A } from '@angular/cdk/keycodes';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-weather',
-  imports: [CdkAriaLive],
+  imports: [CdkAriaLive, AsyncPipe],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
 
 export class Weather {
-  forecast: WeatherData[] = [];
+  forecast$: Observable<WeatherData[]>;
+
   constructor(http: HttpClient) {
-    http.get<WeatherData[]>('http://localhost:5201/weatherforecast').subscribe(result => {  
-      this.forecast = result;
-    });
+    this.forecast$ = http.get<WeatherData[]>('http://localhost:5201/weatherforecast');
   }
 }
