7824d5f70b72d457ff38ee6881610c96c0a68c2c
philippm01 <philippmuro@gmail.com>
2025-12-05 18:20:46 -0800

    week14

--- src/app/auth/auth-service.ts
+++ src/app/auth/auth-service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { LoginResponse } from './login-response';
+import { LoginRequest } from './login-request';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
+
+  private token = 'auth_token';
+  constructor(private http: HttpClient) { }
+
+  login(loginRequest: LoginRequest): Observable<LoginResponse>{
+    return this.http.post<LoginResponse>(environment.apiUrl + 'api/admin', loginRequest)
+    .pipe(tap(response =>{
+      if(response.success) {
+        localStorage.setItem(this.token, response.token);
+      }
+    }));
+  } 
+
+  logout(): void {
+    localStorage.removeItem(this.token);
+  }
+
+  isLoggedIn(): boolean {
+    return localStorage.getItem(this.token) !== null;
+  }
   
 }
